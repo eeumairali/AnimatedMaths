f0649f5474ab4f62a3c69016b21359d6c65dc234
Umair Ali <56534384+eeumairali@users.noreply.github.com>
2026-07-04 06:36:45 +0000

    commit changes

--- LinearEquations/ex3_point_slope.py
+++ LinearEquations/ex3_point_slope.py
@@ -1,30 +1,22 @@
-''' y = slop * x + intercept'''
-
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.title("Point-Slope: y - y1 = m(x - x1)")
 
+m = st.slider("slope m", -5.0, 5.0, 1.0, 0.1)
+x1 = st.slider("x1", -10.0, 10.0, 0.0, 0.5)
+y1 = st.slider("y1", -10.0, 10.0, 0.0, 0.5)
 
-st.title("Linear Equation: y = m·x + b")
+x = np.linspace(-10, 10, 100)
+y = m * (x - x1) + y1
 
-
-m = st.slider("slope m",  -5.0 , 5.0, 1.0, 0.1 )
-b = st.slider("intercept b",  -10.0 , 10.0, 0.0, 0.1 )
-
-
-x= np.linspace(-10,10,100)
-y = m * x + b 
-
-
-fig,ax = plt.subplots()
-ax.plot(x,y, color="#6B6BFA")
-ax.axhline(0,color="gray",lw=0.5)
-ax.axvline(0,color="gray",lw=0.5)
-
-ax.set_xlim(-10,10)
-ax.set_ylim(-10,10)
-
+fig, ax = plt.subplots()
+ax.plot(x, y, color="#378ADD")
+ax.plot(x1, y1, "o", ms=12, color="red")
+ax.axhline(0, color="gray", lw=0.5)
+ax.axvline(0, color="gray", lw=0.5)
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
 ax.grid(True, alpha=0.3)
-
 st.pyplot(fig)
